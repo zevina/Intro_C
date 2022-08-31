ffafe4afde142a335085bad17b9db2d3f8038c02
lena zevina <zevina.l@yandex.ru>
2022-08-31 14:58:20 +0700

    Add Example005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+  Console.Write("Ура, это же Маша!");
+}
+else
+{
+  Console.Write("Привет, " + username + "!");
+}
