24d89e040ae2021d6d9e83431a3b2d16f8297a49
lena zevina <zevina.l@yandex.ru>
2022-08-31 18:40:05 +0700

    Add Example007 Magic v2.0

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,15 +2,45 @@
 // Console.SetCursorPosition(10, 4);
 // Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 80, yc = 30;
+int xa = 100, ya = 1,
+    xb = 1, yb = 90,
+    xc = 200, yc = 90; 
 
 Console.SetCursorPosition(xa, ya); 
-Console.WriteLine("+");
+Console.WriteLine("▲");
 
 Console.SetCursorPosition(xb, yb); 
-Console.WriteLine("+");
+Console.WriteLine("▲");
 
 Console.SetCursorPosition(xc, yc); 
-Console.WriteLine("+");
+Console.WriteLine("▲");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 100000)
+{
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("▲");
+    count++;
+}
