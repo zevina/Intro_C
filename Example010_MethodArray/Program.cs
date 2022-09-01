9e113c82f956999a3393e78f1900c708ebed4964
lena zevina <zevina.l@yandex.ru>
2022-09-01 18:59:14 +0700

    add example010 method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿//             0   1   2  3   4   5   6  7
+int[] array = {1, 52, 68, 4, 45, 68, 47, 8};
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    System.Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
