29bb819611493f68ad08db6938eb6eea52c995f7
lena zevina <zevina.l@yandex.ru>
2022-09-01 19:20:53 +0700

    add example011 array library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  while (index < length)
+  {
+    collection[index] = new Random().Next(1,10);
+    index++;
+  }
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  while (position < count)
+  {
+    System.Console.WriteLine(col[position]);
+    position++;
+  }
+}
+
+int IndexOf(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;
+  while (index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+if (pos == -1)
+{
+  System.Console.WriteLine("Заданное число отсутствует в массиве");
+}
+else  System.Console.WriteLine(pos);
